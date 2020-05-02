a5d753b9bfc32aff79273b1aee3b6aa6fd216947
AndreOA <a.atalaia@campus.fct.unl.pt>
2020-05-02 18:42:53 +0100

    Added the new tests to unit.h

--- code/src/unit.h
+++ code/src/unit.h
@@ -3,11 +3,41 @@
 
 typedef void (*TESTFUNCTION)(void *, size_t, size_t,int);
 
-extern TESTFUNCTION testFunction[];
+extern TESTFUNCTION testFunction1[];
 
-extern char *testNames[];
+extern char *testNames1[];
 
-extern int nTestFunction;
+extern int nTestFunction1;
+
+
+extern TESTFUNCTION testFunction2[];
+
+extern char *testNames2[];
+
+extern int nTestFunction2;
+
+
+extern TESTFUNCTION testFunction3[];
+
+extern char *testNames3[];
+
+extern int nTestFunction3;
+
+
+
+extern TESTFUNCTION testFunction4[];
+
+extern char *testNames4[];
+
+extern int nTestFunction4;
+
+
+
+extern TESTFUNCTION testFunction5[];
+
+extern char *testNames5[];
+
+extern int nTestFunction5;
 
 
 typedef void (*VALIDATEFUNCTION)(void *, size_t, size_t);
